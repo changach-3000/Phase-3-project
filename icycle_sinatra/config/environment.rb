678de64e808caff8c091bf3536732ef5caa39b51
changach-3000 <sharon.changach@student.moringaschool.com>
2023-06-08 13:17:11 +0300

    backend push

--- icycle_sinatra/config/environment.rb
+++ icycle_sinatra/config/environment.rb
@@ -3,4 +3,11 @@ ENV["RACK_ENV"] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
+#database configuration
+configure do 
+    set :database,{
+        adapter: "sqlite3",
+        database: "db/development.sqlite3"
+    }
+end
 require_all "app"
