c5f2e99640d3fdf6a20a58f6dc9ab84435104c78
changach-3000 <sharon.changach@student.moringaschool.com>
2023-06-09 08:40:55 +0300

    added some changes

---
+++ README.md
@@ -0,0 +1,35 @@
+# iCYCLE
+iCYCLE is an application for cyclists where they can come, connect and desicover new routes to explore.
+
+# MVP
+So fo this project, these are the deliverables it needs to meet
+## Normal user:
+    Sign up / Log In / reset password
+    View available routes
+    Add posts/Delete posts/ Edit posts
+    View profile
+
+## Admin
+    Sign-up/login/reset password
+    Approve posts
+    Add routes/ edit / delete routes
+
+## Project Setup
+You can fork this project into your local machine and then run the front end and backend 
+For the frontend, you can first run an npm install to install all the packages, and for the backend run a bundle install
+
+## Project Deliverables
+For this project, you must:
+
+- Use Active Record to interact with a database.
+- Have a minimum of two models with a one-to-many relationship.
+- Create API routes in Sinatra that handles at least three different CRUD actions for at least one of your Active Record models.
+- Build a separate React frontend application that interacts with the API to perform CRUD actions.
+- Use good OO design patterns. You should have separate classes for each of your models, and create instance and class methods as necessary.
+
+
+# Prerequisites
+Vscode or any othe IDE
+
+## License
+This Project is licenced by [MIT](https://choosealicense.com/licenses/mit/)
--- icycle_frontend/src/components/Profile.jsx
+++ icycle_frontend/src/components/Profile.jsx
@@ -2,11 +2,12 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
 function Profile() {
-  const {currentuser}= useContext(AuthContext)
-  return (
-    <div> 
 
-      <div class="container my-5">
+  const {currentuser}= useContext(AuthContext)
+  
+  return (
+    
+       <div class="container my-5">
                 <div class="row justify-content-center">
                     <div class="col-md-2">
                         <div class="profile-img">
@@ -29,44 +30,10 @@ function Profile() {
                     <div class="col-md-2">
                         <input type="submit" class="btn btn-outline-success" value="Edit Profile"/>
                     </div>
-                </div>
-                     
+                </div> 
         </div>
+                
 
-        <main class="main-container mb-3">  
-            <div class="main-title mb-3">
-                <p style={{"textAlign":"center"}}>USER DASHBOARD</p>
-            </div>
-            <div class="main-cards">
-            <div className='row'>
-              <div className="col-md-1">
-
-              </div>
-                <div class="card col-md-3 me-3">
-                    <div class="card-inner">
-                        <p class="text-primary">DISTANCES COVERED</p>
-                        <span class="material-icons-outlined text-blue">directions_bike</span>
-                    </div>
-                    <span class="text-primary font-weight-bold">80km</span>
-                </div>
-                <div class="card col-md-3 me-3">
-                    <div class="card-inner">
-                        <p class="text-primary">AVERAGE TIME CYCLING</p>
-                        <span class="material-icons-outlined text-orange">timer</span>
-                    </div>
-                    <span class="text-primary font-weight-bold">20hrs</span>
-                </div>
-                <div class="card col-md-3 me-3">
-                    <div class="card-inner">
-                        <p class="text-primary">POSTS</p>
-                        <span class="material-icons-outlined text-green">add_a_photo</span>
-                    </div>
-                    <span class="text-primary font-weight-bold">12</span>
-                </div>
-                </div>
-            </div>
-        </main>
-       </div>
   )
 }
 
